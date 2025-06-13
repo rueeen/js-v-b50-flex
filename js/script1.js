4934f4a260c1cbb3329142661fc9100d7f1de324
perico <rva.valencia@gmail.com>
2025-06-13 19:44:11 -0400

    feat: add files for js exercises

---
+++ js/script1.js
@@ -0,0 +1,11 @@
+alert("Hola mundo"); // Esto es un comentario
+
+/**
+ * Comentario 
+ * multi
+ * linea
+ */
+
+console.log("Esto escribe en consola");
+
+document.writeln("Esto escribe en el documento html");
